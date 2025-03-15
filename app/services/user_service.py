97ee27a695a0b76a0a833031d591d02dc6dc7ba6
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2025-03-15 14:19:54 -0300

    use hash_password to encrypt users password

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,11 +1,13 @@
 from app.models.user import User
 from app.schemas.user import UserCreate
 from app.repository import user_repository
+from app.utils.hash import hash_password
 
 
 def create_user(user: UserCreate):
     id = len(user_repository.get_all_users()) + 1
-    new_user = User(id=id, nome=user.nome, email=user.email, senha=user.senha)
+    hashed_senha = hash_password(user.senha)
+    new_user = User(id=id, nome=user.nome, email=user.email, senha=hashed_senha)
     return user_repository.create_user(new_user)
 
 
